522c96cc5a84df1f724bbe14e820eb3378dcbebe
Tatyana Belova <tatyana.belova.21.09.1999@mail.ru>
2023-08-04 12:30:18 +0300

    8 kyu 1. Summation 2. Opposite number 3. Convert a Number to a String! 4. Convert boolean values to strings 'Yes' or 'No' 5. Find the smallest integer in the array

---
+++ CodeWars/04.08.23.js
@@ -0,0 +1,40 @@
+//Summation
+var summation = function (num) {
+  let sum = 0;
+
+  for (i = 1; i <= num; i++) {
+    sum = sum + i
+  }
+  return sum
+}
+
+// Альтернативный вариант решения с без цикла (взято в обсуждений CodeWars)
+var summation1 = function (num) {
+    return num * (num + 1) / 2
+}
+
+//Opposite number
+function opposite(number) {
+  return number * (-1)
+}
+
+//Convert a Number to a String!
+function numberToString(num) {
+  return String(num);
+}
+
+// Convert boolean values to strings 'Yes' or 'No'.
+function boolToWord( bool ){
+  return bool ? 'Yes' : 'No'
+}
+
+//Find the smallest integer in the array
+function findSmallestInt(args) {
+  let min = args[0];
+  for (let i = 1; i < args.length; i++ ) {
+      if (args[i] < min) {
+        min = args[i]
+    }
+  }
+  return min
+}
